286385b93f6df5beb870a0296c528206a0d6611f
ray <raynescofield@gmail.com>
2025-02-18 11:03:28 +0800

    blink.cmp cmdline sources

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -56,14 +56,14 @@ return {
       -- with blink.compat
       compat = {},
       default = { "lsp", "path", "snippets", "buffer" },
-      cmdline = function()
-        local type = vim.fn.getcmdtype()
-        -- Search forward and backward
-        if type == "/" or type == "?" then return { "buffer" } end
-        -- Commands
-        if type == ":" or type == "@" then return { "cmdline" } end
-        return {}
-      end,
+      -- cmdline = function()
+      --   local type = vim.fn.getcmdtype()
+      --   -- Search forward and backward
+      --   if type == "/" or type == "?" then return { "buffer" } end
+      --   -- Commands
+      --   if type == ":" or type == "@" then return { "cmdline" } end
+      --   return {}
+      -- end,
       min_keyword_length = 0,
       providers = {
         lsp = {
